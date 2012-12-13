7b52b2a88ae56682f66a1639e60dd9822cb766ae
David Durost <david.durost@gmail.com>
2012-12-13 09:40:53 -0500

    added views

--- src/Landmarx/LandmarkBundle/Controller/LandmarkController.php
+++ src/Landmarx/LandmarkBundle/Controller/LandmarkController.php
@@ -5,32 +5,74 @@ namespace Landmarx\LandmarkBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
 
 class LandmarkController extends Controller
 {
+    /**
+     * 
+     * @return render
+     * @throws NotFoundException
+     */
     public function indexAction()
     {
         // get geo location of user
         // -- if logged in maybe store it in session to get faster?
       
-        $landmarks = $this->get('doctrine_mongodb')
-                          ->getRepository('LandmarxLandmarkBundle:Landmark');
-        // delimite with a radius of the users location
-        if (!$landmarks) {
-          throw $this->createNotFoundException('No landmarks found.');
+        $landmarks = $this->get( 'doctrine_mongodb' )
+                          ->getRepository( 'LandmarxLandmarkBundle:Landmark' );
+        // delimite with a radius of the users location here
+        if ( !$landmarks ) {
+          throw $this->createNotFoundException( 'No landmarks found.' );
         }
+        
+        return $this->render( 'LandmarxLandmarkBundle:Landmark:index.html.twig', array('landmarks' => $landmarks ) );
     }
     
-    public function showAction($slug)
+    /**
+     * 
+     * @param string $slug
+     * @return return
+     * @throws NotFoundException
+     */
+    public function showAction( $slug )
     {
-      $landmark = $this->get('doctrine_mongodb')
-                       ->getRepository('LandmarxLandmarkBundle:Landmark')
-                       ->findBySlug($slug);
+      $landmark = $this->get( 'doctrine_mongodb' )
+                       ->getRepository( 'LandmarxLandmarkBundle:Landmark' )
+                       ->findBySlug( $slug );
       
-      if(!$landmark)
+      if( !$landmark )
       {
-        throw $this->createNotFoundException('No landmark found.');
+        throw $this->createNotFoundException( 'No landmark found.' );
       }
+      
+      return $this->render( 'LandmarxLandmarkBundle:Landmark:show.html.twig', array('landmark' => $landmark ) );
     }
-    
+ 
+    public function newAction( Request $request ) 
+    {
+        $landmark = new Landmark();
+        $form = $this->createFormBuilder( $landmark )
+              ->add('name')
+              ->add('description')
+              ->add('categories')
+              ->add('parent')
+              ->add('latlng', new GoogleMapType())
+              ->getForm();
+
+        if ( $request->getMethod() == 'POST' ) 
+        {
+            $form->bindRequest( $this->getRequest() );
+            if( $form->isValid() ) 
+            {
+                $em = $this->get('doctrine_mongodb')->getManager();
+                $em->persist( $landmark);
+                $em->flush();
+
+                return $this->redirect( $this->generateUrl('landmarks') );
+            }
+        }
+        
+        return $this->render( 'LandmarxLandmarkBundle:Landmark:new.html.twig', array( 'form' => $form->createView() ) );
+    }
 }
--- src/Landmarx/LandmarkBundle/Document/Landmark.php
+++ src/Landmarx/LandmarkBundle/Document/Landmark.php
@@ -4,6 +4,8 @@ namespace Landmarx\LandmarkBundle\Document;
 use Landmarx\Landmark\LandmarkItem;
 use Landmarx\LandmarkBundle\Document\LandmarkCategory;
 use Doctrine\ODM\MongoDB\Mapping\Annotations as MongoDB;
+use Symfony\Component\Validator\Constraints as Assert;
+use Oh\GoogleMapFormTypeBundle\Validator\Constraints as OhAssert;
 
 /**
  * @MongoDB\Document
@@ -34,7 +36,7 @@ class Landmark extends LandmarkItem
   {
     $this->categories = $categories;
     
-    if(!in_array($this->primary_category, $this->categories))
+    if ( !in_array( $this->primary_category, $this->categories ) )
     {
       $this->primary_category = null;
     }
@@ -42,18 +44,35 @@ class Landmark extends LandmarkItem
     return $this;
   }
   
-  public function addCategory($category, $attributes = array()) {
-    if(!$category instanceof LandmarkCategory)
+    public function addCategory( $category, $attributes = array() ) 
     {
-      $category = new LandmarkCategory($category, $attributes);
+        if ( !$category instanceof LandmarkCategory )
+        {
+          $category = new LandmarkCategory($category, $attributes);
+        }
+        $this->categories[ $category->getName() ] = $category;
+
+        return $category;
     }
-    $this->categories[$category->getName()] = $category;
-    
-    return $category;
-  }
-  
-  public function getCategory($name)
-  {
-    return isset($this->categories[$name]) ? $this->categories[$name] : null;
-  }
+
+    public function getCategory($name)
+    {
+        return isset ( $this->categories[ $name ] ) ? $this->categories[ $name ] : null;
+    }
+
+    public function setLatLng($latlng)
+    {
+        $this->setLat( $latlng[ 'lat' ] );
+        $this->setLng( $latlng[ 'lng' ] );
+        return $this;
+    }
+
+    /**
+     * @Assert\NotBlank()
+     * @OhAssert\LatLng()
+     */
+    public function getLatLng()
+    {
+        return array( 'lat' => $this->getLat(), 'lng' => $this->getLng() );
+    }
 }
